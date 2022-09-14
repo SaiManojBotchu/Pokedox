0b301992ac9e8ef641975048efe0c840f3382508
Sai Manoj <saimanoj2826@gmail.com>
2022-09-14 21:41:50 +0530

    finishing touch (styling css)

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -11,8 +11,10 @@ class Pokecard extends Component {
     let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
     return (
       <div className="Pokecard">
-        <h1 className="Pokecard-title">{this.props.name}</h1>
-        <img src={imgSrc} alt={this.props.name}/>
+        <h2 className="Pokecard-title">{this.props.name}</h2>
+        <div className="Pokecard-image">
+          <img src={imgSrc} alt={this.props.name}/>
+        </div>
         <div className="Pokecard-data">Type: {this.props.type}</div>
         <div className="Pokecard-data">Exp: {this.props.exp}</div>
       </div>
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -4,11 +4,16 @@ import './Pokedex.css'
 
 class Pokedex extends Component {
   render () {
+    let title;
+    if(this.props.isWinner) {
+      title = <h3 className="Pokedex-winner">Winning Hand</h3>
+    } else {
+      title = <h3 className="Pokedex-loser">Losing Hand</h3>
+    }
     return (
       <div className="Pokedex">
-        <h1>Pokedex!</h1>
-        <p>Total Experience : {this.props.exp}</p>
-        <p>{this.props.isWinner ? 'Winner' : 'Loser'}</p>
+        {title}
+        <h4>Total Experience : {this.props.exp}</h4>
         <div className="Pokedex-cards"> 
           {this.props.pokemon.map((p) => (
             <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience}/>
